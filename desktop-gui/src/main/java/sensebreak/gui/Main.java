bded20b96fa76c9cd7e8835019878375ffa20f28
Kamilla Ishmukhammedova <ishmukam@fel.cvut.cz>
2025-05-10 17:06:08 +0200

    setup: configure Maven modules, plugins and dependencies

---
+++ desktop-gui/src/main/java/sensebreak/gui/Main.java
@@ -0,0 +1,16 @@
+package sensebreak.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Sense Break GUI");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
